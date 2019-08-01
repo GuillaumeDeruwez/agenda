db4f1dc49bdbd2f114c148beda0a0ec3dd1bb9ee
Guillaume Deruwez <gderuwezdev@gmail.com>
2019-08-01 14:18:28 +0200

    pwa

---
+++ precache-manifest.a8e5c6a1fe8b62f7b1038df7518db5f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/agenda/robots.txt"
+  },
+  {
+    "revision": "8150acfec433290b50d6",
+    "url": "/agenda/js/chunk-vendors.b35d1da4.js"
+  },
+  {
+    "revision": "1caf1ee5a3dd186e46bb",
+    "url": "/agenda/js/app.e1a218ce.js"
+  },
+  {
+    "revision": "5ec52e068d5222e114c99af3215969a2",
+    "url": "/agenda/index.html"
+  },
+  {
+    "revision": "1caf1ee5a3dd186e46bb",
+    "url": "/agenda/css/app.2ed1fee9.css"
+  }
+];
