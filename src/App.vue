26b0898bd7974da72fbb50b05978e9a734c56d0c
Guillaume Deruwez <gderuwezdev@gmail.com>
2019-08-01 14:01:51 +0200

    accessibility part 1

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
